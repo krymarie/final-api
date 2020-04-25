db019d5ded96cb8611c9d0ec5b9571f3b332e26a
Krystal Mitchell <10262399@my.uvu.edu>
2020-04-24 19:49:43 -0600

    post js

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema(
+const clientSchema = new Schema(
   {
     title: {
       type: String,
@@ -24,4 +24,4 @@ const postSchema = new Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model("Post", clientSchema);
